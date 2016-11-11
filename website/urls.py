1b986c084162135771f09d9f6dca9784b4601b08
Gbekeloluwa Olufotebi Simeon <olufotebigbeke@gmail.com>
2016-11-11 09:41:30 +0000

    added base template, routes and views for about and projects

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name='homepage')
+    url(r'^$', views.HomePageView.as_view(), name='homepage'),
+    url(r'^about$', views.AboutPageView.as_view(), name='aboutpage'),
+    url(r'^projects$', views.ProjectsPageView.as_view(), name='projectspage')
+
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,11 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     """docstring for HomePageView"""
     template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ProjectsPageView(TemplateView):
+    template_name = 'projects.html'
